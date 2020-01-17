ba0f49e86bc2b70e1ab35ddfccf7e661c4074811
Alan Filipe Cardozo Fabeni <alanfcf@gmail.com>
2020-01-17 13:00:01 -0300

    first commit

---
+++ Scottish Power Tech Test/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Scottish Power Tech Test
+//
+//  Created by Alan Filipe Cardozo Fabeni on 17/01/20.
+//  Copyright © 2020 Alan Filipe Cardozo Fabeni. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
